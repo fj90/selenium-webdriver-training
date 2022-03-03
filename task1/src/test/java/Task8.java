e80df7919a5bfcf8d38aa874254bbfcc4ab6cce0
fj90 <fj.90@mail.ru>
2022-03-03 10:18:40 +0400

    Task8
    First attempt

--- task1/src/test/java/Task8.java
+++ task1/src/test/java/Task8.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class Task8 {
     private WebDriver driver;
 
+
     @Before
     public void start() {
         driver = new ChromeDriver();
@@ -23,50 +24,41 @@ public class Task8 {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-
         driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
 
-
-        int counties_ammount = driver.findElements(By.cssSelector("#content > form > table > tbody > tr > td:nth-child(5) > a")).size();
+        int counties_ammount = driver.findElements(By.cssSelector("#content td:nth-child(5) a")).size();
         char[] char_arr = new char[counties_ammount];
 
         for (int i = 2; i < (counties_ammount); i++) {
-            char_arr[i] = driver.findElement(By.cssSelector("#content > form > table > tbody > tr:nth-child(" + i + ") > td:nth-child(5) > a")).getAttribute("textContent").charAt(0);
-           String S= driver.findElement(By.cssSelector("#content > form > table > tbody > tr:nth-child(" + i + ") > td:nth-child(6)")).getAttribute("textContent");
+            char_arr[i] = driver.findElement(By.cssSelector("#content tr:nth-child(" + i + ") td:nth-child(5) a")).getAttribute("textContent").charAt(0);
+            String S = driver.findElement(By.cssSelector("#content tr:nth-child(" + i + ") td:nth-child(6)")).getAttribute("textContent");
             System.out.println(S);
             boolean equals = S.equals("0");
             System.out.println(equals);
-        if (equals== false) {
-             driver.findElement(By.cssSelector("#content > form > table > tbody > tr:nth-child(" + i + ") > td:nth-child(7)")).click();
-
-            int zones_ammount= driver.findElements(By.cssSelector("#table-zones > tbody > tr > td:nth-child(3)")).size();
-            char[] char_arr_zones = new char[zones_ammount];
-
-            for (int j = 2; j < (zones_ammount); j++) {
-
-                char_arr_zones[j] =driver.findElement(By.cssSelector("#table-zones > tbody > tr:nth - child("+j+") > td:nth - child(3)")).getAttribute("textContent").charAt(0);
+            if (equals == false) {
+                driver.findElement(By.cssSelector("#content tr:nth-child(" + i + ") td:nth-child(7)")).click();
+                int zones_ammount = driver.findElements(By.cssSelector("#table-zones td:nth-child(3)")).size();
+                char[] char_arr_zones = new char[zones_ammount];
+                for (int j = 2; j < (zones_ammount); j++) {
+                    char_arr_zones[j] = driver.findElement(By.cssSelector("#table-zones tr:nth-child(" + j + ") td:nth-child(3)")).getText().charAt(0);
+                    char[] nonSortedArray = Arrays.copyOfRange(char_arr, 2, zones_ammount);
+                    char[] SortedArray = Arrays.copyOfRange(char_arr, 2, zones_ammount);
+                    Arrays.sort(SortedArray);
+                    if (Arrays.equals(nonSortedArray, SortedArray) == false) {
+                        throw new Exception("Countries are not in alphabetical order");
+                    }
+                }
+                System.out.println(char_arr_zones + "- zone");
+                driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
             }
-            System.out.println(char_arr_zones);
-            driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
         }
-
-        }
-
 
         char[] nonSortedArray = Arrays.copyOfRange(char_arr, 2, counties_ammount);
-        char[] SortedArray = Arrays.copyOfRange(char_arr,2, counties_ammount);
+        char[] SortedArray = Arrays.copyOfRange(char_arr, 2, counties_ammount);
         Arrays.sort(SortedArray);
-        if (Arrays.equals(nonSortedArray, SortedArray) == false) {throw new Exception("Countries are not in alphabetical order");}
-
-
-        char[] CHARS = {'O', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        Arrays.sort(CHARS);
-
-
-        int i = 1;
-
-
+        if (Arrays.equals(nonSortedArray, SortedArray) == false) {
+            throw new Exception("Countries are not in alphabetical order");
+        }
     }
 
     @After
@@ -75,3 +67,4 @@ public class Task8 {
         driver = null;
     }
 }
+
