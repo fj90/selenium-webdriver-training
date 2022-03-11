8819200581fd6ae4cc23e08c2501dac2f792b99e
fj90 <fj.90@mail.ru>
2022-03-11 18:29:56 +0400

    Task 12
    task 12

--- task1/src/test/java/Task13.java
+++ task1/src/test/java/Task13.java
@@ -27,38 +27,35 @@ public class Task13 {
         for (int g = 1; g < 4; g++) {
             driver.findElement(By.cssSelector("#box-most-popular li:nth-child(1)")).click();
 
-            if ( driver.findElements(By.cssSelector("#box-product select")).size()!=0) {
+            if ( driver.findElements(By.cssSelector("#box-product select")).size()!=0) { //если есть выбор размера утки
                 driver.findElement(By.cssSelector("#box-product select")).click();
                 driver.findElement(By.cssSelector("[value=Small]")).click();
             }
 
             driver.findElement(By.cssSelector("#box-product button")).click();
-            System.out.println(driver.findElement(By.cssSelector("#cart span.quantity")).getText());
-            driver.findElement(By.cssSelector("#cart > a.content > span.quantity")).getText();
+            driver.findElement(By.cssSelector("#cart span.quantity")).getText();
             wait.until(ExpectedConditions.attributeToBe(driver.findElement(By.cssSelector("#cart span.quantity")), "innerText", String.valueOf(g)));
             driver.get("http://localhost/litecart/");
-            System.out.println( "g="+g);
         }
 
-        driver.findElement(new By.ByLinkText("Checkout »")).click();
+            driver.findElement(new By.ByLinkText("Checkout »")).click(); //переход в корзину
+            driver.findElement(By.cssSelector("[name='cart_form']")).click();
+
+            int item_amount = driver.findElements(By.cssSelector("ul.items li")).size();
+            for (int i = 0; i < item_amount; i++) {
+            int line_count = driver.findElements(By.cssSelector("div#box-checkout-summary tr")).size(); // число строк в таблице
+             driver.findElement(By.cssSelector("li.item:nth-child(1) button[name='remove_cart_item']")).click(); //  Remove
+            wait.until((WebDriver driver) -> driver.findElements(By.cssSelector("div#box-checkout-summary tr")).size() < line_count);
+            System.out.println("товар номер" +i+ "удален");
+
+        }
 
 
-        while (driver.findElements(By.cssSelector("#box-checkout-cart [value=Remove]")).size()!=0) {
-                String previous_value = driver.findElement(By.cssSelector("table tr.footer td:nth-child(2)")).getText();
-                System.out.println("previous_value =" + previous_value);
-                wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#box-checkout-cart [value=Remove]")))).click();
 
-                if (driver.findElements(By.cssSelector("#order_confirmation-wrapper table")).size() != 0) {
 
-                    wait.until(ExpectedConditions.attributeToBe(driver.findElement(By.cssSelector("table tr.footer td:nth-child(2)")), "innerText", previous_value));
-                   // System.out.println(driver.findElement(By.cssSelector("new value" + driver.findElement(By.cssSelector("table tr.footer td:nth-child(2)")))));
-                    //  int i =1;
-                }
+
             }
-
-
-            //int j =1;
-    }
+            
 
 
     @After
