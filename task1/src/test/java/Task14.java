ce496497edd16e246e87ce0b2c5ab6fad35b8b8f
fj90 <fj.90@mail.ru>
2022-03-11 15:54:41 +0400

    Task 14
    first attempt

---
+++ task1/src/test/java/Task14.java
@@ -0,0 +1,77 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Task14 {
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test() {
+        WebDriverWait wait = new WebDriverWait(driver, 100);
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        driver.findElement(new By.ByLinkText("Countries")).click();
+        driver.findElement(new By.ByCssSelector("#content tr:nth-child(2) td:nth-child(7)")).click();
+        
+        String mainWindow = driver.getWindowHandle();
+        Set<String> oldWindows = driver.getWindowHandles();
+        List<WebElement> ExternalLinks = driver.findElements(By.cssSelector(".fa-external-link"));
+
+        for (int i = 2; i < ExternalLinks.size()+4; i++) {
+            if (i==4) {i=i++;  continue;}
+            if (i==5) {i=i++;  continue;}
+            driver.findElement(new By.ByCssSelector("#content tr:nth-child(" +i+ ") i.fa.fa-external-link")).click();
+            String newWindow = wait.until(anyWindowOtherThan(oldWindows));
+            driver.switchTo().window(newWindow);
+            driver.close();
+            System.out.println( "Окно с порядковым номером " + i + " закрыто");
+            driver.switchTo().window(mainWindow);
+        }
+
+        }
+
+    public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
+        return new ExpectedCondition<String>() {
+            public String apply(WebDriver driver) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(oldWindows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
+        };
+    }
+
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
