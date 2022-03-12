67565e75ebea65c35da750abbef901864c10975c
fj90 <fj.90@mail.ru>
2022-03-12 15:32:12 +0400

    Task 8
    Task 8 rework

--- task1/src/test/java/Task8.java
+++ task1/src/test/java/Task8.java
@@ -30,35 +30,35 @@ public class Task8 {
 
         int counties_ammount = driver.findElements(By.cssSelector("#content td:nth-child(5) a")).size();
         ArrayList<String> str_arr = new ArrayList<>();
-        ArrayList<String> str_arr_sort;
+        ArrayList<String> str_arr_sort = new ArrayList<>();
         ArrayList<String> str_arr_zones = new ArrayList<>();
-        ArrayList<String> str_arr_zones_sort;
 
-        for (int i = 2; i <= (counties_ammount); i++) {
+        for (int i = 2; i <= (counties_ammount + 1); i++) {
             str_arr.add(driver.findElement(By.cssSelector("#content tr:nth-child(" + i + ") td:nth-child(5) a")).getAttribute("textContent"));
 
             String S = driver.findElement(By.cssSelector("#content tr:nth-child(" + i + ") td:nth-child(6)")).getAttribute("textContent");
             boolean equals = S.equals("0");
-          //  System.out.println(equals);
             if (equals == false) {
+                ArrayList<String> str_arr_zones_sort = new ArrayList<>();
                 driver.findElement(By.cssSelector("#content tr:nth-child(" + i + ") td:nth-child(7)")).click();
                 int zones_ammount = driver.findElements(By.cssSelector("#table-zones td:nth-child(3)")).size();
                 for (int j = 2; j <= (zones_ammount); j++) {
                     str_arr_zones.add(driver.findElement(By.cssSelector("#table-zones tr:nth-child(" + j + ") td:nth-child(3)")).getText());
                 }
-
-                str_arr_zones_sort=str_arr_zones;
+                for (int k = 0; k < str_arr_zones.size(); k++) {
+                    str_arr_zones_sort.add(str_arr_zones.get(k));
+                }
                 Arrays.sort(new ArrayList[]{str_arr_zones_sort});
-                Assert.assertEquals( "Cортировка зон не по алфавиту", str_arr_zones, str_arr_zones_sort);
+                Assert.assertEquals("Cортировка зон не по алфавиту", str_arr_zones, str_arr_zones_sort);
 
                 driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
             }
         }
-        str_arr_sort = str_arr;
+        for (int k = 0; k < str_arr.size(); k++) {
+            str_arr_sort.add(str_arr.get(k));
+        }
         Arrays.sort(new ArrayList[]{str_arr_sort});
-        Assert.assertEquals( "Cортировка стран не по алфавиту", str_arr, str_arr_sort);
-
-
+        Assert.assertEquals("Cортировка стран не по алфавиту", str_arr, str_arr_sort);
     }
 
     @After
